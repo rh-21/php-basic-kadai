7f752047627bb9de3e2f9aa40ef3f22ef2620ea8
rh-21 <riho.a21@gmail.com>
2024-12-05 23:05:02 +0900

    再提出

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -12,17 +12,17 @@
         // ソートする配列を宣言
             $nums = array(15, 4, 18, 23, 10 );
 
-            function sort_2way($nums ,$order){
+            function sort_2way($array ,$order){
                 if ($order === 'TRUE'){
                     echo '昇順にソートします。<br>';
-                    asort($nums);
+                    asort($array);
                 }elseif ($order === 'FALSE'){
                     echo '降順にソートします。<br>';
-                    arsort($nums);
+                    arsort($array);
                 }
 
-                foreach ($nums as $num) {
-                    echo "{$num}<br>";
+                foreach ($array as $deta) {
+                    echo "{$deta}<br>";
                 }
             }
             sort_2way($nums ,'TRUE');
