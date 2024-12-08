65ca654b6744f4c6b201274e6097e3a923756584
rh-21 <riho.a21@gmail.com>
2024-12-09 00:57:05 +0900

    課題16追加

---
+++ kadai_016/class.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP 課題16</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // クラスを定義する
+         class Food {
+             // プロパティを定義する
+             private $name;
+             private $price;
+
+             public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+         }
+        }
+
+         class Animal {
+             // プロパティを定義する
+             public $name;
+             public $height;
+             public $weight;
+
+             public function __construct(string $name, int $height ,int $weight ) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+         }
+        }
+
+        
+         // インスタンス化する
+         
+         $show_price = new Food('potaro',250);
+         $show_height = new Animal ('dog' , 60 , 5000);
+         
+         print_r($show_price);
+         echo '<br>';
+         print_r($show_height);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
