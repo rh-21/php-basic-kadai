132316d8b40ee0faa8ef04ba510a53cc3f12fe89
rh-21 <riho.a21@gmail.com>
2024-12-12 09:28:19 +0900

    __constructに変更(初回と同様)

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -1,21 +1,46 @@
 <!DOCTYPE html>
-<html lang="jp">
-<head>
-    <meta charset="UTF-8">
-    <title>kadai-11
-    </title>
-</head>
-<body>
-    <p>
-        <?php 
-        $foods = ['名前' => '玉ねぎ' , '値段' => '200' , '産地' => '北海道' ] ;
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP 課題16</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // クラスを定義する
+         class Food {
+             // プロパティを定義する
+             private $name;
+             private $price;
 
-        foreach ($foods as $key => $value){ 
-            echo "{$key}：{$value}" . '<br>' ;
+             public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+         }
         }
-        ?>
-    </p>
 
-</body>
-</html>
+         class Animal {
+             // プロパティを定義する
+             public $name;
+             public $height;
+             public $weight;
 
+             public function __construct(string $name, int $height ,int $weight ) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+         }
+        }
+         $show_price = new Food('potaro',250);
+         $show_height = new Animal ('dog' , 60 , 5000);
+         
+         print_r($show_price);
+         echo '<br>';
+         print_r($show_height);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
