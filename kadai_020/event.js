aa1445c43dcba8652759251128a54361769916d8
rh-21 <riho.a21@gmail.com>
2025-02-03 16:56:58 +0900

    課題20追加

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const addBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+addBtn.addEventListener("click", () => {
+    text.textContent = "ボタンをクリックしました";
+});
+
